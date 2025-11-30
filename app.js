2b9ac390a58a72a807e7fe63c437f4cde43efc52
ahmadieya <eyaahmadi029@gmail.com>
2025-12-01 00:12:38 +0100

    creer serveur

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const http = require('http'); // importer http
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,12 +22,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,4 +37,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+const server = http.createServer(app); //sna3et serveur
+server.listen(3000, () => {
+  console.log("app is running on port 3000");
+
+});
