fa239cc935b439afa21e450fc3a1a949bfd970aa
ahmadieya <eyaahmadi029@gmail.com>
2025-12-01 20:39:04 +0100

    config

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require("dotenv").config();
+require("dotenv").config();//les ifos sensible
 const http = require('http'); // importer http
 const{connectToMongo}=require ('./config/db')
+
 var indexRouter = require('./routes/indexRouter');
 var usersRouter = require('./routes/usersRouter');
+
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 module.exports.connectToMongo = async () => {
     mongoose.set('strictQuery', false);
-    mongoose.connect(process.env.MongoDb_URL)
+    mongoose.connect(process.env.MongoDb_URL) //lien mtaa base 
     .then(
         () => { console.log("connect to db") }
     )
